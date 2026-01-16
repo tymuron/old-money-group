f335094cff46a211bd0bd555e714072d0dacc0ce
Ganna Chystiakova <annaromeo@Gannas-MacBook-Air.local>
2026-01-16 10:42:38 +0100

    UI Fix: Moved separator to between Asset Management and Concierge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,11 +13,11 @@ const Home = () => {
             <Navbar />
             <Hero />
             <BookingWidget />
-            {/* Elegant Separator - More Visible */}
-            <div className="w-full h-16 bg-omg-cream flex items-center justify-center">
-                <div className="w-1/2 h-[2px] bg-omg-gold/50"></div>
-            </div>
             <OwnerRegistry />
+            {/* Elegant Separator - Between Asset Management & Concierge */}
+            <div className="w-full h-16 bg-omg-green flex items-center justify-center">
+                <div className="w-1/2 h-[1px] bg-omg-gold/30"></div>
+            </div>
             <ContactSection />
             <Footer />
         </Layout>
